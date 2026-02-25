28005981387a172a55c6bfcecb105e36a43f8c07
Avinash365 <akjhaharauli@gmail.com>
2026-02-26 01:01:56 +0530

    fix: url issue

--- legal-sthal-crm/src/lib/axios.ts
+++ legal-sthal-crm/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'; 
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, // Replace with your API base URL
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL, // Replace with your API base URL
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
