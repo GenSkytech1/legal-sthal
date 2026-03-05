f7b851184627c9f0d9b75a121240dd3808f4c189
Avinash365 <akjhaharauli@gmail.com>
2026-03-06 03:00:20 +0530

    feat:service and plan

--- legal-sthal-crm-backend/routes/api.php
+++ legal-sthal-crm-backend/routes/api.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\Api\CompanySettingController;
 use App\Http\Controllers\WebsiteContentController;
 use App\Http\Controllers\CustomPageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\Service\ServiceController;
+use App\Http\Controllers\API\Service\PlanController;
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -29,3 +32,26 @@ Route::middleware(['jwt.cookie', 'auth:api'])->group(function () {
 
 Route::get('/webhooks/meta', [MetaWebhookController::class, 'verify']);
 Route::post('/webhooks/meta', [MetaWebhookController::class, 'handle']);
+
+
+// Service Routes
+Route::middleware(['jwt.cookie', 'auth:api'])->prefix('services')->group(function () {
+
+    Route::get('/', [ServiceController::class,'index']);
+    Route::post('/store', [ServiceController::class,'store']);
+    Route::post('/update/{id}', [ServiceController::class,'update']);
+    Route::delete('/delete/{id}', [ServiceController::class,'destroy']);
+
+});
+
+// Plan Routes
+Route::middleware(['jwt.cookie', 'auth:api'])->prefix('plans')->group(function(){
+
+    Route::get('/',[PlanController::class,'index']);
+    Route::get('/{id}',[PlanController::class,'show']);
+    Route::post('/store',[PlanController::class,'store']);
+    Route::post('/update/{id}',[PlanController::class,'update']);
+    Route::delete('/delete/{id}',[PlanController::class,'destroy']);
+    Route::delete('/delete-multiple',[PlanController::class,'deleteMultiple']);
+
+});
